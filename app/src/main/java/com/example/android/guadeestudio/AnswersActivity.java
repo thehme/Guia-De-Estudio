5e1990cf4cb07735d480963aa1acc1122c5d4e8a
thehme <bcortez23@gmail.com>
2018-06-04 23:09:13 -0400

    added logic to restart button in answers page

--- app/src/main/java/com/example/android/guadeestudio/AnswersActivity.java
+++ app/src/main/java/com/example/android/guadeestudio/AnswersActivity.java
@@ -1,12 +1,25 @@
 package com.example.android.guadeestudio;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class AnswersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.answers_activity);
+
+        // attach event listener to start Button
+        Button actionButton = (Button) findViewById(R.id.restart_button);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent actionIntent = new Intent(AnswersActivity.this, MainActivity.class);
+                startActivity(actionIntent);
+            }
+        });
     }
 }
