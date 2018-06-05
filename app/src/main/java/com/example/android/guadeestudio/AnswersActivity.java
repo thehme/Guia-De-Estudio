fab167121eb4dc82e4cbce7d3e51ee761e56d63f
thehme <bcortez23@gmail.com>
2018-06-04 21:45:53 -0400

    new files for answers activity page

---
+++ app/src/main/java/com/example/android/guadeestudio/AnswersActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.guadeestudio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AnswersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.answers_activity);
+    }
+}
